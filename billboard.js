ee7af5046f5047e17c9adedc0e362d3ba91e3361
Pavel Gorbokon <pahanix@gmail.com>
2012-06-14 23:31:47 +0700

    Week of June 23, 2012

--- billboard.js
+++ billboard.js
@@ -1,7 +1,7 @@
 var Chartianity = {
   chartName : "Billboard 200",
-  publishDate : "June 16, 2012",
-  items : [{"position":15,"artist_title":"For Today","media_title":"Immortal","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drs100/s198/s19853n8ve3.jpg"},{"position":27,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":36,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":129,"artist_title":"Laura Story","media_title":"Blessings","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drp900/p910/p91034ot6n1.jpg"},{"position":151,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"}],
+  publishDate : "June 23, 2012",
+  items : [{"position":45,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":52,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":58,"artist_title":"The Rocket Summer","media_title":"Life Will Write The Words","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drs600/s669/s66992p0cef.jpg"},{"position":183,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"},{"position":197,"artist_title":"newsboys","media_title":"God's Not Dead","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drq800/q888/q88863pdv3o.jpg"}],
   
   generateHTML : function(imageSize){
     var html = "<table><thead><tr><th colspan=\"4\">" + 
