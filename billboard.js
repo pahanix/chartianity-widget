d133350ec22d12ba52fa8d00eb7f7c44fba486e1
Pavel Gorbokon <pahanix@gmail.com>
2012-07-05 18:25:50 +0530

    Week of July 14, 2012

--- billboard.js
+++ billboard.js
@@ -1,7 +1,7 @@
 var Chartianity = {
   chartName : "Billboard 200",
-  publishDate : "June 30, 2012",
-  items : [{"position":58,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":73,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":144,"artist_title":"Sidewalk Prophets","media_title":"Live Like That","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":149,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"},{"position":156,"artist_title":"Skillet","media_title":"Awake","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drn200/n218/n21875onqu6.jpg"},{"position":200,"artist_title":"Building 429","media_title":"Listen To The Sound","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drp900/p998/p99848lz73q.jpg"}],
+  publishDate : "July 14, 2012",
+  items : [{"position":86,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":100,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":200,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"}],
   
   generateHTML : function(imageSize){
     var html = "<table><thead><tr><th colspan=\"4\">" + 
