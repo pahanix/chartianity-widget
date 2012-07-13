52e787a8b9f7c9e5041bdd9a60300ea3ba5de98a
Pavel Gorbokon <pahanix@gmail.com>
2012-07-13 12:47:08 +0300

    Week of July 21, 2012

--- billboard.js
+++ billboard.js
@@ -1,7 +1,7 @@
 var Chartianity = {
   chartName : "Billboard 200",
-  publishDate : "July 14, 2012",
-  items : [{"position":86,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":100,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":200,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"}],
+  publishDate : "July 21, 2012",
+  items : [{"position":45,"artist_title":"newsboys","media_title":"God's Not Dead","media_url":"http://www.billboard.com/images/album_images/cov200/pop/cov200/drq800/q888/q88863pdv3o.jpg"},{"position":122,"artist_title":"MercyMe","media_title":"The Hurt & The Healer","media_url":"http://www.billboard.com/images/defaults/artist-94.gif"},{"position":136,"artist_title":"Mary Mary","media_title":"Go Get It (Soundtrack)","media_url":"http://www.billboard.com/images/defaults/album-94.gif"},{"position":198,"artist_title":"Casting Crowns","media_title":"Come To The Well","media_url":"http://www.newreleasetuesday.com/images/album_cover_images/cover_10491.jpg"}],
   
   generateHTML : function(imageSize){
     var html = "<table><thead><tr><th colspan=\"4\">" + 
